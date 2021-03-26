e94d71eb78c68706f73442cb917f7fdba528454f
John Cokins <il7148pg@go.minnstate.edu>
2021-03-25 22:32:57 -0500

    buttons added, links between pages

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -5,5 +5,7 @@ from . import views
 # points requests made to homepage should be handled by place_list in views module
 urlpatterns = [
     path('', views.place_list, name='place_list'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('place/<int:place_pk>/was_visited', views.place_was_visited, name='place_was_visited'),
+    path('visited', views.places_visited, name='places_visited')
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 # Create your views here.
@@ -19,7 +19,24 @@ def place_list(request):
     new_place_form = NewPlaceForm()  # used to create HTML
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
+
 def about(request):
     author = 'John'
     about = 'A website to create a list of places to visit'
     return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
+
+
+def places_visited(request):
+    visited = Place.objects.filter(visited=True)
+    return render(request, 'travel_wishlist/visited.html', {'visited' : visited})
+
+
+# database query, getting object with pk == int passed by url
+def place_was_visited(request, place_pk):
+    if request.method == 'POST':
+        #place = Place.objects.get(pk=place_pk)
+        place = get_object_or_404(Place, pk=place_pk)
+        place.visited = True
+        place.save()
+
+    return redirect('place_list')  # name of the path
